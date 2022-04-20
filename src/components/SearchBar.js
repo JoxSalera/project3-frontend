06f078b3167db6ac7ac1458603b1678f54906bfc
lb2718 <98560887+lb2718@users.noreply.github.com>
2022-04-20 10:45:16 +0200

    search bar done

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState();
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate({ pathname: "/", search: `?q=${searchTerm}` });
+  };
   return (
-    <div>
-      <input placeholder="Search location"></input>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search location"
+        onChange={handleChange}
+        value={searchTerm}
+      ></input>
+    </form>
   );
 };
 
