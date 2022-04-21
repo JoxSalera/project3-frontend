d4b8e59ef98056cfde307f382f6f7797a6b6c63f
lb2718 <98560887+lb2718@users.noreply.github.com>
2022-04-21 13:02:50 +0200

    log out button done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
+import { AuthContext } from "../context/auth.context";
 
 import "./Navbar.css";
 import "../App.css";
 
 const Navbar = () => {
+  const { logOutUser, isLoggedIn } = useContext(AuthContext);
+
   return (
     <div>
       <nav>
@@ -18,6 +22,11 @@ const Navbar = () => {
         <Link className="homepage" to="/profile">
           <span>PROFILE</span>
         </Link>
+        {isLoggedIn && (
+          <>
+            <button onClick={logOutUser}>Log out</button>
+          </>
+        )}
       </nav>
     </div>
   );
